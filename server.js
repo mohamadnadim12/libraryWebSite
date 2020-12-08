dbbca18b79b854eda9a5ba25ed9025559706ff59
Mohamad <mohammednadim12@gmail.com>
2020-12-08 17:03:14 +0400

    Initial Author Routes

--- server.js
+++ server.js
@@ -4,13 +4,18 @@ if (process.env.NODE_ENV != "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
+
+//routs
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
+
 app.set("view engine", "ejs");
 app.set("views", "./views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
-
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
 const db = mongoose.connection;
@@ -18,4 +23,5 @@ db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 app.listen(process.env.PORT || 3000);
