cfa7005ba075df0f2c2ae63a08c4128d34e31976
Mohamad <mohammednadim12@gmail.com>
2020-12-27 16:48:58 +0400

    Initial Booksr Routes

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const bodyParser = require("body-parser");
 //routs
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 
 app.set("view engine", "ejs");
 app.set("views", "./views");
@@ -24,4 +25,5 @@ db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
 app.use("/authors", authorRouter);
+app.use("/books", bookRouter);
 app.listen(process.env.PORT || 3000);
