42f4875089e2a2f41008a364c53ab6a2ff377d9b
Mohamad <mohammednadim12@gmail.com>
2020-12-29 00:22:24 +0400

    author routes

--- public/javascripts/fileUpload.js
+++ public/javascripts/fileUpload.js
@@ -1,12 +1,13 @@
 FilePond.registerPlugin(
   FilePondPluginImagePreview,
   FilePondPluginImageResize,
-  FilePondPluginFileEncode
-);
+  FilePondPluginFileEncode,
+)
 
 FilePond.setOptions({
   stylePanelAspectRatio: 150 / 100,
   imageResizeTargetWidth: 100,
-  imageResizeTargetHeight: 150,
-});
+  imageResizeTargetHeight: 150
+})
+
 FilePond.parse(document.body);
--- routes/books.js
+++ routes/books.js
@@ -1,81 +1,78 @@
-const express = require("express");
-const router = express.Router();
-
-const Book = require("../models/book");
-const Author = require("../models/author");
-
-const imageMimeTypes = ["image/jpeg", "image/png", "images/gif"];
+const express = require('express')
+const router = express.Router()
+const Book = require('../models/book')
+const Author = require('../models/author')
+const imageMimeTypes = ['image/jpeg', 'image/png', 'images/gif']
 
 // All Books Route
-router.get("/", async (req, res) => {
-  let query = Book.find();
-  if (req.query.title != null && req.query.title != "") {
-    query = query.regex("title", new RegExp(req.query.title, "i")); // i means dont care if it's lowercase or uppercase;
+router.get('/', async (req, res) => {
+  let query = Book.find()
+  if (req.query.title != null && req.query.title != '') {
+    query = query.regex('title', new RegExp(req.query.title, 'i'))
   }
-  if (req.query.publishedBefore != null && req.query.publishedBefore != "") {
-    query = query.lte("publishDate", req.query.publishedBefore); // lte mean less than
+  if (req.query.publishedBefore != null && req.query.publishedBefore != '') {
+    query = query.lte('publishDate', req.query.publishedBefore)
   }
-  if (req.query.publishedAfter != null && req.query.publishedAfter != "") {
-    query = query.gte("publishDate", req.query.publishedAfter);
+  if (req.query.publishedAfter != null && req.query.publishedAfter != '') {
+    query = query.gte('publishDate', req.query.publishedAfter)
   }
   try {
-    const books = await query.exec(); // execute the queres above
-    res.render("books/index", {
+    const books = await query.exec()
+    res.render('books/index', {
       books: books,
-      searchOptions: req.query,
-    });
+      searchOptions: req.query
+    })
   } catch {
-    res.redirect("/");
+    res.redirect('/')
   }
-});
+})
 
 // New Book Route
-router.get("/new", async (req, res) => {
-  renderNewPage(res, new Book());
-});
+router.get('/new', async (req, res) => {
+  renderNewPage(res, new Book())
+})
 
 // Create Book Route
-router.post("/", async (req, res) => {
+router.post('/', async (req, res) => {
   const book = new Book({
     title: req.body.title,
     author: req.body.author,
     publishDate: new Date(req.body.publishDate),
     pageCount: req.body.pageCount,
-    description: req.body.description,
-  });
-
-  saveCover(book, req.body.cover);
+    description: req.body.description
+  })
+  saveCover(book, req.body.cover)
 
   try {
-    const newBook = await book.save();
+    const newBook = await book.save()
     // res.redirect(`books/${newBook.id}`)
-    res.redirect(`books`);
+    res.redirect(`books`)
   } catch {
-    renderNewPage(res, book, true);
+    renderNewPage(res, book, true)
   }
-});
+})
 
 async function renderNewPage(res, book, hasError = false) {
   try {
-    const authors = await Author.find({});
+    const authors = await Author.find({})
     const params = {
       authors: authors,
-      book: book,
-    };
-    if (hasError) params.errorMessage = "Error Creating Book";
-    res.render("books/new", params);
+      book: book
+    }
+    if (hasError) params.errorMessage = 'Error Creating Book'
+    res.render('books/new', params)
   } catch {
-    res.redirect("/books");
+    res.redirect('/books')
   }
 }
 
 function saveCover(book, coverEncoded) {
-  if (coverEncoded == null) return;
-  const cover = JSON.parse(coverEncoded);
+  if (coverEncoded == null) return
+  const cover = JSON.parse(coverEncoded)
   if (cover != null && imageMimeTypes.includes(cover.type)) {
-    book.coverImage = new Buffer.from(cover.data, "base64");
-    book.coverImageType = cover.type;
+    book.coverImage = new Buffer.from(cover.data, 'base64')
+    book.coverImageType = cover.type
   }
 }
 
-module.exports = router;
+module.exports = router
